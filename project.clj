39b8c3006f466924e7ae96987832b5d2982482e3
Florian Kretlow <florian.kretlow@innoq.com>
2023-06-10 21:10:34 +0200

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.fkretlow/router "0.1.0-SNAPSHOT"
+(defproject com.fkretlow/router "0.1.0"
   :description "request routing library for use with the Ring framework"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/fkretlow/router"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]])
